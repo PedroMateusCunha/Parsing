f337b650ce89eb55a4780df469af65bf41a23b01
Pedro Cunha <pedrokunhapimenta1234@gmail.com>
2019-09-18 01:42:50 -0300

    First Commit

---
+++ parsing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+	echo "#######################"
+	echo "#  Analisador de URL  #"
+	echo "#######################"	
+	echo "Modo de uso: ./par.sh <url> "
+	echo "By: Pedro Cunha"
+else
+	wget -q  $1
+	grep href index.html | cut -d "/" -f3 | grep "\." | cut -d '"' -f1 | sort -u | grep -v '<li' > lista
+	for url in $(cat lista);do host $url | grep "has address"; done
+	rm lista
+	rm index.html
+fi
